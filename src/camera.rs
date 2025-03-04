b8ecff490a8282236ac0bfdc6a6cdb1fd56229ea
Tung Nguyen <tungtn3@gmail.com>
2025-03-04 19:47:06 +1100

    Simplifying the Ray-Sphere Intersection Code

--- src/camera.rs
+++ src/camera.rs
@@ -4,15 +4,15 @@ use crate::vec3::*;
 
 fn hit_sphere(center: &Vec3, radius: f64, r: &Ray) -> Option<f64> {
     let oc = *center - r.pos;
-    let a = r.dir.dot(r.dir);
-    let b = -2.0 * r.dir.dot(oc);
-    let c = oc.dot(oc) - radius * radius;
-    let discriminant = b * b - 4.0 * a * c;
+    let a = r.dir.length_squared();
+    let h = r.dir.dot(oc);
+    let c = oc.length_squared() - radius * radius;
+    let discriminant = h * h - a * c;
 
     if discriminant < 0.0 {
         None
     } else {
-        Some((-b - discriminant.sqrt()) / (2.0 * a))
+        Some((h - discriminant.sqrt()) / a)
     }
 }
 
